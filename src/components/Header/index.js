320a0732a0961de3dec4cf9ec2adb9b656d0b3d5
Alana Ragusa <alana.ragusarabello@gmail.com>
2021-11-22 22:32:42 -0300

    header almost done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,14 +6,14 @@ import logoEbac from '../../images/logo-ebac.svg'
 const Header = () => {
     return (
         <header className="header__container">
-            <div className="header__logo--state">
+            <div className="header__logo header__logo--state">
                 <img src={logoAcessaBr} alt="Logo AcessaBR"/>
                 <span className="header__city"> 
                     / São Paulo - SP
                 </span>
             </div>
             <div className="header__logo--ebac">
-                <span>
+                <span className="header__span">
                     Apoio:
                 </span>
                 <img src={logoEbac} alt="Logo Ebac"/>
