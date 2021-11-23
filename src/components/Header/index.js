d9c75253647cba3412b47723330e8e9d16cdb348
Alana Ragusa <alana.ragusarabello@gmail.com>
2021-11-22 21:58:39 -0300

    update header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import 'style';
+import logoAcessaBr from '../../images/logo-acessaBR.svg'
+import logoEbac from '../../images/logo-ebac.svg'
 
 const Header = () => {
     return (
         <header className="header__container">
-
+            <div className="header__logo--state">
+                <img src={logoAcessaBr} alt="Logo AcessaBR"/>
+            </div>
+            <div className="header__logo--ebac">
+                <img src={logoEbac} alt="Logo Ebac"/>
+            </div>
         </header>
     )
 }
+
+export default Header;
