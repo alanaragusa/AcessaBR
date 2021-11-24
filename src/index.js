e619a6ad7dc921281013dd9bd82ef6ae912ae570
Alana Ragusa <alana.ragusarabello@gmail.com>
2021-11-23 23:24:20 -0300

    city name ok

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './styles/global.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
 
+const CONFIG = {
+  state: 'SP',
+  city: 'São Paulo'
+}
+
 ReactDOM.render(
   <div className="main__wrapper">
     <div className="main__container">
-      <Header/>
-      <Home/>
+      <Header city={CONFIG.city} state={CONFIG.state} />
+      <Home city={CONFIG.city} />
     </div>
   </div>
   ,
