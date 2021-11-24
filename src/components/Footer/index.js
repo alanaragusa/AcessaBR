90c93de9940a028766c4ad02c9da9388e9aa2795
Alana Ragusa <alana.ragusarabello@gmail.com>
2021-11-24 19:49:05 -0300

    started footer component files

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.scss';
+import iconTop from '../../images/icon-top.png';
+
+function Footer() {
+    return(
+        <footer className="footer__container">
+            <div className="footer__section footer__section--first">
+
+            </div>
+            
+            <div className="footer__section footer__section--second">
+
+            </div>
+        </footer>
+    )
+}
