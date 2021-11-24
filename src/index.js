7fca536f5c1bf45b741f0891e513c2598f77f73c
Alana Ragusa <alana.ragusarabello@gmail.com>
2021-11-24 20:08:31 -0300

    ok footer component js file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './styles/global.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Curator from './components/Curator';
+import Footer from './components/Footer';
 
 const CONFIG = {
   state: 'SP',
@@ -17,6 +18,7 @@ ReactDOM.render(
       <Home city={CONFIG.city} />
       
       <Curator />
+      <Footer />
     </div>
   </div>
   ,
