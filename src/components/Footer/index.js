2c40a234a3427068063764c55aba1540d83fbdce
Alana Ragusa <alana.ragusarabello@gmail.com>
2021-11-24 21:19:45 -0300

    scss footer files with margin and padding

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ function Footer() {
     return(
         <footer className="footer">
             <div className="footer__section footer__section--first">
-                <div className="container">
+                <div className="footer__container">
                     <p>
                         <strong>Observação: </strong> este site é uma amostra de desenvolvimento tecnológico de iniciativa independente, as informações aqui apresentadas podem estar eventualmente desatualizadas, por isso recomendamos sempre conferir as informações diretamente com o estabelecimento. A exibição dos locais, nesta página não caracteria recomendação de uso.
                     </p>
@@ -17,7 +17,8 @@ function Footer() {
                 <div className="footer__container">
                     <p>
                         <strong>Projeto Elaborado durante a RDW <a href="https://ebaconline.com" target="_blank" rel="noreferrer" >E.B.A.C</a> 2021</strong>
-                        Projetado por <a href="https://github.com/cavalcantemmarcelo" target="_blank"  rel="noreferrer">Marcelo Cavalcante</a> e <a href="https://github.com/mja-maia"  target="_blank" rel="noreferrer">Miguel Maia</a>, desenvolvido por Aluno EBAC. 
+                        
+                        <small>Projetado por <a href="https://github.com/cavalcantemmarcelo" target="_blank"  rel="noreferrer">Marcelo Cavalcante</a> e <a href="https://github.com/mja-maia"  target="_blank" rel="noreferrer">Miguel Maia</a>, desenvolvido por Aluno EBAC. </small>
                     </p>
                 </div>
             </div>
