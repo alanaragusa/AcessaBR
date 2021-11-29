73e7a922553b3828e5baf54879c6ff4c6ef2c7a2
Alana Ragusa <alana.ragusarabello@gmail.com>
2021-11-29 14:24:15 -0300

    updated files

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/global.scss';
-import Curator from './components/Curator';
 import Footer from './components/Footer';
-import Places from './components/Places';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <div className="main__wrapper">
-    <div className="main__container">
+  <div className="main__container">
       <BrowserRouter>
         <Routes />
-        <Places />
-        <Curator />
       </BrowserRouter>
     </div>
     <Footer />
